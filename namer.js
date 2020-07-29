f2ad01414b6187fa92aac5bf5189a4815e18f0db
Christopher Wong <cwong51799@gmail.com>
2020-07-29 15:08:26 -0400

    Update namer.js

--- namer.js
+++ namer.js
@@ -4,9 +4,34 @@ const client = new Discord.Client({
     partials: ["MESSAGE", "CHANNEL", "REACTION"],
 })
 
-const adjectives = ["biggest", "medium", "smallest", "big", "small", "no"]
+const adjectives = ["abrupt", "acidic", "adorable", "adventurous", "aggressive", "agitated", "alert", "aloof", "amiable", "amused", "annoyed", "antsy", "anxious", "appalling", "appetizing", "apprehensive", "arrogant", "ashamed", "astonishing", "attractive", "average", "batty", "bewildered", "biting", "bitter", "bland", "blushing", "bored", "brave", "bright", "broad", "bulky", "burly", "charming", "cheeky", "cheerful", "chubby", "clean", "clear", "cloudy", "clueless", "clumsy", "colorful", "combative", "comfortable", "condemned", "condescending", "confused", "contemplative", "convincing", "convoluted", "cooperative", "corny", "costly", "courageous", "crabby", "creepy", "crooked", "cruel", "cumbersome", "curved", "cynical", "dangerous", "dashing", "decayed", "deceitful", "deep", "defeated", "defiant", "delicious", "delightful", "depraved", "depressed", "despicable", "determined", "dilapidated", "disgusted", "distinct", "distraught", "distressed", "disturbed", "dizzy", "drab", "drained", "dull", "eager", "ecstatic", "elated", "elegant", "emaciated", "embarrassed", "enchanting", "encouraging", "energetic", "enthusiastic", "envious", "exasperated", "excited", "exhilarated", "extensive", "exuberant", "fancy", "fantastic", "fierce", "filthy", "flat", "floppy", "fluttering", "foolish", "frantic", "fresh", "friendly", "frightened", "frothy", "frustrating", "funny", "fuzzy", "gaudy", "gentle", "ghastly", "giddy", "glamorous", "gleaming", "glorious", "gorgeous", "graceful", "greasy", "grieving", "gritty", "grotesque", "grubby", "grumpy", "handsome", "happy", "harebrained", "healthy", "helpful", "helpless", "high", "hollow", "homely", "horrific", "hungry", "hurt", "icy", "ideal", "impressionable", "intrigued", "irate", "irritable", "itchy", "jealous", "jittery", "jolly", "joyous", "juicy", "jumpy", "kind", "lackadaisical", "lazy", "lethal", "little", "lively", "livid", "lonely", "loose", "lovely", "lucky", "ludicrous", "macho", "magnificent", "maniacal", "melancholy", "melted", "mistaken", "misty", "moody", "mortified", "motionless", "muddy", "mysterious", "narrow", "nasty", "naughty", "nervous", "nonchalant", "nonsensical", "nutritious", "nutty", "obedient", "oblivious", "obnoxious", "odd", "old-fashioned", "outrageous", "panicky", "perplexed", "petite", "petty", "plain", "pleasant", "poised", "pompous", "precious", "prickly", "proud", "pungent", "quaint", "quizzical", "ratty", "reassured", "relieved", "repulsive", "responsive", "ripe", "robust", "rotten", "rotund", "rough", "round", "salty", "sarcastic", "scant", "scary", "scattered", "selfish", "shaggy", "shaky", "shallow", "sharp", "shiny", "silky", "silly", "skinny", "slimy", "slippery", "smarmy", "smiling", "smoggy", "smooth", "smug", "soggy", "solid", "sore", "sour", "sparkling", "spicy", "splendid", "spotless", "square", "stale", "steady", "steep", "sticky", "stormy", "stout", "straight", "strange", "strong", "stunning", "substantial", "successful", "succulent", "superficial", "superior", "swanky", "sweet", "tart", "tasty", "tender", "tense", "terrible", "testy", "thankful", "thick", "thoughtful", "thoughtless", "tight", "timely", "tricky", "trite", "troubled", "twitter", "pated", "uneven", "unsightly", "upset", "uptight", "vexed", "victorious", "virtuous", "vivacious", "vivid", "wacky", "weary", "whimsical", "whopping", "wicked", "witty", "wobbly", "wonderful", "worried", "yummy", "zany", "zealous", "zippy"]
 const nouns = ["pp", "mouth", "belly", "poggers", "simp", "brain", "head", "meme", "butt", "toe", "foot",
                 "cat", "dog", "clam", "rat", "poop", "dinkleberry", "heart"]
+
+
+/* Tier Arrays */
+const tier1 = ["no"]
+const tier2 = ["smallest", "tiniest", "littlest"]
+const tier3 = ["small", "tiny", "little", "miniature"]
+const tier4 = ["medium", "average", "moderate", "normal", "usual", "standard"]
+const tier5 = ["big", "large", "huge", "hefty", "sizable", "fat", "considerable", "substantial", "heavy", "copious"]
+const tier6 = ["biggest", "largest", "hugest", "immense", "enormous", "colossal", "massive", "mammoth", "momentous", "vast", "tremendous"]
+const tier7 = ["best"]
+const tier8 = ["bestest"]
+
+/* Probabilities */
+const tier1Threshold = .25
+const tier2Threshold = .5
+const tier3Threshold = .725
+const tier4Threshold = .905
+const tier5Threshold = .98
+const tier6Threshold = .995
+const tier7Threshold = .999
+const tier8Threshold = 1.0
+
+
+
+
 
 client.on("guildMemberAdd", (member) => {
     nickname(member)
@@ -16,8 +41,8 @@ client.on("guildMemberAdd", (member) => {
 var PREFIX = "!"
 const CHRIS = "110128099361849344";
 const botSpamChannel = "738062969493258392"
-var oddsToGetBest = .005
-var defaultOddsOfRare = .005
+var probabilityMultiplier = 1
+var defaultProabilityMultiplier = 1
 
 client.on("message", (msg) => {
     if(!msg.content.startsWith(PREFIX) || msg.channel.id != botSpamChannel)
@@ -64,14 +89,25 @@ client.on("message", (msg) => {
                 msg.reply("That's illegal.")
                 return
             }
-            oddsToGetBest = parseFloat(args[1])
-            msg.channel.send("```Odds have been changed to " + args[1] + " for " + args[2] + " seconds.```")
+            probabilityMultiplier = parseFloat(args[1])
+            msg.channel.send("```Probability multiplier has been changed to " + args[1] + " for " + args[2] + " seconds.```")
             // Reset after timeout
             setTimeout(function(){ 
-                oddsToGetBest = defaultOddsOfRare
-                msg.channel.send("```Odds have been returned to " + defaultOddsOfRare + "```")
+                probabilityMultiplier = defaultProabilityMultiplier
+                msg.channel.send("```Probability multiplier has been returned to " + defaultProabilityMultiplier + "```")
             }, parseInt(args[2]) * 1000);
             break;
+        case "probabilities":
+            msg.channel.send("```" + "Probabilities:" + "\n\n" + 
+                parsePercentageFromFloat(tier1Threshold/tier1.length) + " : " + tier1 +"\n" + 
+                parsePercentageFromFloat((tier2Threshold-tier1Threshold)/tier2.length) + " : " + tier2 +"\n" + 
+                parsePercentageFromFloat((tier3Threshold-tier2Threshold)/tier3.length) + " : " + tier3 +"\n" + 
+                parsePercentageFromFloat((tier4Threshold-tier3Threshold)/tier4.length) + " : " + tier4 +"\n" + 
+                parsePercentageFromFloat((tier5Threshold-tier4Threshold)/tier5.length) + " : " + tier5 +"\n" + 
+                parsePercentageFromFloat((tier6Threshold-tier5Threshold)/tier6.length) + " : " + tier6 +"\n" + 
+                parsePercentageFromFloat((tier7Threshold-tier6Threshold)/tier7.length) + " : " + tier7 +"\n" + 
+                parsePercentageFromFloat((tier8Threshold-tier7Threshold)/tier8.length) + " : " + tier8 +"\n-dan wu```")
+            break
         case "ping":
             msg.reply("Pong!")
             break
@@ -82,7 +118,7 @@ client.on("message", (msg) => {
  * @param {member} - The member object to be renamed
  */
 function nickname(member){
-    var newName = getAnAdjective() + " " + getANoun() + " " + getAnAdjective() + " " + getANoun()
+    var newName = getASize() + " " + getANoun() + " " + getAnAdjective() + " " + getANoun()
     if (newName.length >= 32) {
         newName = newName.substring(0,32)
     }
@@ -95,21 +131,53 @@ function nickname(member){
     return newName
 }
 
+
+function getASize(){
+    var chanceRoll = Math.random() * probabilityMultiplier
+    if (chanceRoll < tier1Threshold) {
+        return chooseRandom(tier1)
+    }
+    if (chanceRoll < tier2Threshold) {
+        return chooseRandom(tier2)
+    }
+    if (chanceRoll < tier3Threshold) {
+        return chooseRandom(tier3)
+    }
+    if (chanceRoll < tier4Threshold) {
+        return chooseRandom(tier4)
+    }
+    if (chanceRoll < tier5Threshold) {
+        return chooseRandom(tier5)
+    }
+    if (chanceRoll < tier6Threshold) {
+        return chooseRandom(tier6)
+    }
+    if (chanceRoll < tier7Threshold) {
+        return chooseRandom(tier7)
+    } else {
+        return chooseRandom(tier8)
+    }
+}
+
+/**
+ * Parses a float into a string % rounded to the hundredths place
+ */
+function parsePercentageFromFloat(float){
+    return  (Math.round(100*(float*100))/100).toString() + " %"
+}
+
+/**
+ * Choose a random element from the array
+ */
+function chooseRandom(array){
+    return array[Math.floor(Math.random()*array.length)]
+}
 /**
  * Grabs a label from nouns
  * @returns An adjective
  */
 function getAnAdjective(){
-    var chanceRoll = Math.random()
-    if (chanceRoll < oddsToGetBest) {
-        if(chanceRoll < oddsToGetBest/2) {
-            return "bestest"
-        } else {
-            return "best"
-        }
-    } else {
-        return adjectives[Math.floor(Math.random() * adjectives.length)]
-    }
+    return adjectives[Math.floor(Math.random() * adjectives.length)]
 }
 
 /**
@@ -133,3 +201,4 @@ client.on("ready", () => {
  * Log into the bot profile.
  */
 client.login("NzM3MTUyNjM4MTgwNzg2MjY3.Xx5M5A.BtIm3K9CcGl4j4zrfALO_ObMNVw")
+
