434953553c0bbafdbc43949213f9e0aec3a793f7
Christopher Wong <cwong51799@gmail.com>
2020-08-12 12:28:04 -0400

    Added the option to check the chance of your name
    Added the option to check the chance of your name

--- namer.js
+++ namer.js
@@ -7,18 +7,18 @@ const client = new Discord.Client({
 
 const adjectives = ["abrupt", "acidic", "adorable", "adventurous", "aggressive", "agitated", "alert", "aloof", "amiable", "amused", "annoyed", "antsy", "anxious", "appalling", "appetizing", "apprehensive", "arrogant", "ashamed", "astonishing", "attractive", "average", "batty", "bewildered", "biting", "bitter", "bland", "blushing", "bored", "brave", "bright", "broad", "bulky", "burly", "charming", "cheeky", "cheerful", "chubby", "clean", "clear", "cloudy", "clueless", "clumsy", "colorful", "combative", "comfortable", "condemned", "condescending", "confused", "contemplative", "convincing", "convoluted", "cooperative", "corny", "costly", "courageous", "crabby", "creepy", "crooked", "cruel", "cumbersome", "curved", "cynical", "dangerous", "dashing", "decayed", "deceitful", "deep", "defeated", "defiant", "delicious", "delightful", "depraved", "depressed", "despicable", "determined", "dilapidated", "disgusted", "distinct", "distraught", "distressed", "disturbed", "dizzy", "drab", "drained", "dull", "eager", "ecstatic", "elated", "elegant", "emaciated", "embarrassed", "enchanting", "encouraging", "energetic", "enthusiastic", "envious", "exasperated", "excited", "exhilarated", "extensive", "exuberant", "fancy", "fantastic", "fierce", "filthy", "flat", "floppy", "fluttering", "foolish", "frantic", "fresh", "friendly", "frightened", "frothy", "frustrating", "funny", "fuzzy", "gaudy", "gentle", "ghastly", "giddy", "glamorous", "gleaming", "glorious", "gorgeous", "graceful", "greasy", "grieving", "gritty", "grotesque", "grubby", "grumpy", "handsome", "happy", "harebrained", "healthy", "helpful", "helpless", "high", "hollow", "homely", "horrific", "hungry", "hurt", "icy", "ideal", "impressionable", "intrigued", "irate", "irritable", "itchy", "jealous", "jittery", "jolly", "joyous", "juicy", "jumpy", "kind", "lackadaisical", "lazy", "lethal", "little", "lively", "livid", "lonely", "loose", "lovely", "lucky", "ludicrous", "macho", "magnificent", "maniacal", "melancholy", "melted", "mistaken", "misty", "moody", "mortified", "motionless", "muddy", "mysterious", "narrow", "nasty", "naughty", "nervous", "nonchalant", "nonsensical", "nutritious", "nutty", "obedient", "oblivious", "obnoxious", "odd", "old-fashioned", "outrageous", "panicky", "perplexed", "petite", "petty", "plain", "pleasant", "poised", "pompous", "precious", "prickly", "proud", "pungent", "quaint", "quizzical", "ratty", "reassured", "relieved", "repulsive", "responsive", "ripe", "robust", "rotten", "rotund", "rough", "round", "salty", "sarcastic", "scant", "scary", "scattered", "selfish", "shaggy", "shaky", "shallow", "sharp", "shiny", "silky", "silly", "skinny", "slimy", "slippery", "smarmy", "smiling", "smoggy", "smooth", "smug", "soggy", "solid", "sore", "sour", "sparkling", "spicy", "splendid", "spotless", "square", "stale", "steady", "steep", "sticky", "stormy", "stout", "straight", "strange", "strong", "stunning", "substantial", "successful", "succulent", "superficial", "superior", "swanky", "sweet", "tart", "tasty", "tender", "tense", "terrible", "testy", "thankful", "thick", "thoughtful", "thoughtless", "tight", "timely", "tricky", "trite", "troubled", "twitter", "pated", "uneven", "unsightly", "upset", "uptight", "vexed", "victorious", "virtuous", "vivacious", "vivid", "wacky", "weary", "whimsical", "whopping", "wicked", "witty", "wobbly", "wonderful", "worried", "yummy", "zany", "zealous", "zippy"]
 const nouns = ["pp", "mouth", "belly", "poggers", "simp", "brain", "head", "meme", "butt", "toe", "foot",
-                "cat", "dog", "clam", "rat", "poop", "dinkleberry", "heart"]
+                "cat", "dog", "clam", "rat", "poop", "dinkleberry", "heart", "nut", "cashew"]
 
 
 /* Tier Arrays */
-const tier1 = ["no"]
+const tier1 = ["no", "sad"]
 const tier2 = ["smallest", "tiniest", "littlest"]
 const tier3 = ["small", "tiny", "little", "miniature"]
-const tier4 = ["medium", "average", "moderate", "normal", "usual", "standard"]
+const tier4 = ["medium", "average", "moderate", "normal", "usual", "standard", "nutty"]
 const tier5 = ["big", "large", "huge", "hefty", "sizable", "fat", "considerable", "substantial", "heavy", "copious"]
 const tier6 = ["biggest", "largest", "hugest", "immense", "enormous", "colossal", "massive", "mammoth", "momentous", "vast", "tremendous"]
-const tier7 = ["best"]
-const tier8 = ["bestest"]
+const tier7 = ["best", "rare", "happy"]
+const tier8 = ["legendary", "rarest", "pak"]
 
 /* Probabilities */
 const tier1Threshold = .25
@@ -30,7 +30,14 @@ const tier6Threshold = .995
 const tier7Threshold = .999
 const tier8Threshold = 1.0
 
-
+const chanceOfTier1 = tier1Threshold/tier1.length
+const chanceOfTier2 = (tier2Threshold-tier1Threshold)/tier2.length
+const chanceOfTier3 = (tier3Threshold-tier2Threshold)/tier3.length
+const chanceOfTier4 = (tier4Threshold-tier3Threshold)/tier4.length
+const chanceOfTier5 = (tier5Threshold-tier4Threshold)/tier5.length
+const chanceOfTier6 = (tier6Threshold-tier5Threshold)/tier6.length
+const chanceOfTier7 = (tier7Threshold-tier6Threshold)/tier7.length
+const chanceOfTier8 = (tier8Threshold-tier7Threshold)/tier8.length
 
 
 
@@ -42,6 +49,7 @@ client.on("guildMemberAdd", (member) => {
 var PREFIX = "!"
 const CHRIS = "110128099361849344";
 const botSpamChannel = "738062969493258392"
+const safeRole = "humans"
 var probabilityMultiplier = 1
 var defaultProabilityMultiplier = 1
 
@@ -50,6 +58,14 @@ client.on("message", (msg) => {
         return
     let args = msg.content.substring(PREFIX.length).split(" ")
     switch(args[0]) {
+        case "chance":
+            var chance = figureOutNameChance(msg.member)
+            if (chance != -1) {
+                msg.reply("```The chance of receiving " + msg.member.displayName+ " is " + chance + parsePercentageFromFloat(chance) + ".```")
+            } else {
+                msg.reply("```I couldn't find the chance for your name. This is probably because I haven't named you.```")
+            }
+            break
         case "nickname":
             try{
                 msg.reply("You have been renamed to **" + nickname(msg.member) +"**")
@@ -82,8 +98,12 @@ client.on("message", (msg) => {
                 msg.reply("That's illegal.")
                 return
             }
-            msg.guild.members.cache.forEach(member => nickname(member))
-            msg.reply("Renaming everyone. Please wait.")
+            msg.guild.members.cache.forEach((member) => {
+                if(member.id != CHRIS){
+                    nickname(member)
+                }
+            })
+            msg.reply("```Renaming " + msg.guild.memberCount + " users. Please wait.```")
             break
         case "setProbability":
             if (msg.author.id != CHRIS){
@@ -100,14 +120,15 @@ client.on("message", (msg) => {
             break;
         case "probabilities":
             msg.channel.send("```" + "Probabilities:" + "\n\n" + 
-                parsePercentageFromFloat(tier1Threshold/tier1.length) + " : " + tier1 +"\n" + 
-                parsePercentageFromFloat((tier2Threshold-tier1Threshold)/tier2.length) + " : " + tier2 +"\n" + 
-                parsePercentageFromFloat((tier3Threshold-tier2Threshold)/tier3.length) + " : " + tier3 +"\n" + 
-                parsePercentageFromFloat((tier4Threshold-tier3Threshold)/tier4.length) + " : " + tier4 +"\n" + 
-                parsePercentageFromFloat((tier5Threshold-tier4Threshold)/tier5.length) + " : " + tier5 +"\n" + 
-                parsePercentageFromFloat((tier6Threshold-tier5Threshold)/tier6.length) + " : " + tier6 +"\n" + 
-                parsePercentageFromFloat((tier7Threshold-tier6Threshold)/tier7.length) + " : " + tier7 +"\n" + 
-                parsePercentageFromFloat((tier8Threshold-tier7Threshold)/tier8.length) + " : " + tier8 +"\n-dan wu```")
+                "Tier 1- " + parsePercentageFromFloat(chanceOfTier1) + " : " + tier1 +"\n" + 
+                "Tier 2- " + parsePercentageFromFloat(chanceOfTier2) + " : " + tier2 +"\n" + 
+                "Tier 3- " + parsePercentageFromFloat(chanceOfTier3) + " : " + tier3 +"\n" + 
+                "Tier 4- " + parsePercentageFromFloat(chanceOfTier4) + " : " + tier4 +"\n" + 
+                "Tier 5- " + parsePercentageFromFloat(chanceOfTier5) + " : " + tier5 +"\n" + 
+                "Tier 6- " + parsePercentageFromFloat(chanceOfTier6) + " : " + tier6 +"\n" + 
+                "Tier 7- " + parsePercentageFromFloat(chanceOfTier7) + " : " + tier7 +"\n" + 
+                "Tier 8- " + parsePercentageFromFloat(chanceOfTier8) + " : " + tier8 +"\n" +
+                "-dan wu```")
             break
         case "ping":
             msg.reply("Pong!")
@@ -130,6 +151,59 @@ function nickname(member){
         console.log(error)
     }
     return newName
+}
+
+function figureOutNameChance(member) {
+        var authorNameParts = member.displayName.split(" ")
+        console.log(authorNameParts)
+        var currentChance = 1.00
+        for (var i=0;i<authorNameParts.length;i++) {
+            var chanceOfWord = chanceOfGettingWord(authorNameParts[i])
+            console.log(authorNameParts[i], chanceOfWord)
+            if (chanceOfWord != -1) {
+                currentChance = currentChance * chanceOfWord
+            } else {
+                return -1
+            }
+        }
+        return currentChance
+}
+
+/**
+ * If the word is in a tier, adjectives, or nouns array and return the chance of getting it.
+ * If it is in no tier, return -1, indicating an improper name.
+ * @param {String} word 
+ */
+function chanceOfGettingWord(word) {
+    if (tier1.includes(word)) {
+        return chanceOfTier1
+    } else if (tier2.includes(word)) {
+        return chanceOfTier2
+    } else if (tier3.includes(word)) {
+        return chanceOfTier3
+    } else if (tier4.includes(word)) {
+        return chanceOfTier4
+    }
+    else if (tier5.includes(word)) {
+        return chanceOfTier5
+    }
+    else if (tier6.includes(word)) {
+        return chanceOfTier6
+    }
+    else if (tier7.includes(word)) {
+        return chanceOfTier7
+    }
+    else if (tier8.includes(word)) {
+        return chanceOfTier8
+    } else if (adjectives.includes(word)) {
+        return 1/adjectives.length
+    } else if (nouns.includes(word)) {
+        return 1/nouns.length
+    }
+    else {
+        return -1
+    }
+
 }
 
 
@@ -164,7 +238,7 @@ function getASize(){
  * Parses a float into a string % rounded to the hundredths place
  */
 function parsePercentageFromFloat(float){
-    return  (Math.round(100*(float*100))/100).toString() + " %"
+    return  (Math.round(100*(float*100))/100).toString() + "%"
 }
 
 /**
